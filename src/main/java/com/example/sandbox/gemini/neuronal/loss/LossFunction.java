dd02d6a4e29ea20a4e93c8c7ef3a1e4986720e9a
Janos Sarkoezi <j.sarkoezi@googlemail.com>
2025-08-23 14:14:08 +0200

    Package gemini geloescht

--- src/main/java/com/example/sandbox/gemini/neuronal/loss/LossFunction.java
+++
@@ -1,6 +0,0 @@
-package com.example.sandbox.gemini.neuronal.loss;
-
-public interface LossFunction {
-    double calculateLoss(double[] predicted, double[] expected);
-    double[] calculateLossGradient(double[] predicted, double[] expected);
-}
