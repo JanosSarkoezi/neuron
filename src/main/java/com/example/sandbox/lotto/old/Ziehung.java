49c737500818180a05a6c49318a73401b9941499
Janos Sarkoezi <j.sarkoezi@googlemail.com>
2025-10-04 18:56:35 +0200

    gemini delete old directories

--- src/main/java/com/example/sandbox/lotto/old/Ziehung.java
+++
@@ -1,6 +0,0 @@
-package com.example.sandbox.lotto.old;
-
-import java.time.LocalDate;
-import java.util.List;
-
-public record Ziehung(LocalDate tagDerZiehung, List<Integer> zahlen, Integer zusatz, Integer superzahl) {}
--- src/main/java/com/example/sandbox/markov/old/StateProbability.java
+++
@@ -1,3 +0,0 @@
-package com.example.sandbox.markov.old;
-
-public record StateProbability(int state, double probability) {}
